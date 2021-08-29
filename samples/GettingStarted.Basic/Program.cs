842b4a698a0655d02470e08ed871447e56b0ceb2
Mayuki Sawatari <mayuki+github@misuzilla.org>
2021-08-29 12:37:37 +0900

    Update sample

--- samples/GettingStarted.Basic/Program.cs
+++ samples/GettingStarted.Basic/Program.cs
@@ -8,7 +8,13 @@ namespace GettingStarted.Basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Chalk.BgYellow.White + ("Hello " + (Chalk.BrightBlue.Underline + "Konnichiwa") + "!"));
+            Console.WriteLine(string.Join(" ", Chalk.Bold["Bold"], Chalk.Underline["Underline"], Chalk.Inverse["Inverse"]));
+            Console.WriteLine(string.Join(" ", Chalk.Red["Red"], Chalk.Green["Green"], Chalk.Blue["Blue"], Chalk.BgRed["BgRed"], Chalk.BgGreen["BgGreen"], Chalk.BgBlue["BgBlue"]));
+            Console.WriteLine();
+
+            Console.WriteLine(Chalk.Red.Underline["Hello"]);
+            Console.WriteLine(Chalk.Red.Underline["Hello " + Chalk.Underline.BgBlue["World"] + "!"]);
+            Console.WriteLine(Chalk.Bold.Gray.BgYellow + ("＼" + (Chalk.White.BgRed["Hello"] + " " + (Chalk.White.BgBlue + "コンニチハ") + "!!／")));
         }
     }
 }
